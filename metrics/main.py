533ae86821156ebc72a22ec08f285bd1b08b8303
Nikolskiy <nikolskiyvladimir@vk.com>
2021-12-28 16:13:00 +0300

    Draft 28.12.21

---
+++ metrics/main.py
@@ -0,0 +1,4 @@
+import c_index
+from sklearn.metrics import mean_absolute_error, brier_score_loss
+
+# ToDo: Добавить возможность выбирать метрики: декоратор @use_metrics([...])
---
+++ models/best_model.py
@@ -0,0 +1,8 @@
+"""
+Набор функций для сравнения моделей
+"""
+from typing import List
+
+
+def model_cmp(*models):
+    pass
